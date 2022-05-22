fdee3c2036d7b49cd100650ea98a4935d5989aae
Tsukina-7mochi <tsukina7mochi@gmail.com>
2022-05-23 00:08:25 +0900

    再生成の機能を追加

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,26 @@ import generate from './generate';
 import "./style.scss";
 
 window.addEventListener('load', () => {
+  let resultStr = generate();
+
+  const renderResult = function(): void {
+    const resultDiv = document.querySelector('#result > span');
+
+    if(!resultDiv) {
+      throw Error('Element "#result > span" does not exist.');
+    }
+
+    resultDiv.textContent = resultStr;
+  }
+
+  document.querySelector('#generate')?.addEventListener('click', () => {
+    resultStr = generate();
+
+    renderResult();
+  });
+
   document.querySelector('#tweet')?.addEventListener('click', () => {
-    const tweetText = `${generate()}\n#パワーマンハッタン大学チャレンジ\nhttps://tsukina-7mochi.github.io/power-manhattan-challenge/`;
+    const tweetText = `${resultStr}\n#パワーマンハッタン大学チャレンジ\nhttps://tsukina-7mochi.github.io/power-manhattan-challenge/`;
 
     const link = document.createElement('a');
     link.href = `https://twitter.com/intent/tweet?text=${encodeURIComponent(tweetText)}`;
@@ -12,4 +30,6 @@ window.addEventListener('load', () => {
 
     link.click();
   });
+
+  renderResult();
 });
